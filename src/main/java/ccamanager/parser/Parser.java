cb6df3da8028b98e77a2922014208e9324af5f31
yy43yy <yiyang15624@gmail.com>
2026-03-24 01:42:51 +0800

    changed import style

--- src/main/java/ccamanager/parser/Parser.java
+++ src/main/java/ccamanager/parser/Parser.java
@@ -1,6 +1,15 @@
 package ccamanager.parser;
 
-import ccamanager.command.*;
+import ccamanager.command.AddCcaCommand;
+import ccamanager.command.AddResidentCommand;
+import ccamanager.command.AddResidentToCcaCommand;
+import ccamanager.command.ExitCommand;
+import ccamanager.command.Command;
+import ccamanager.command.UnknownCommand;
+import ccamanager.command.DeleteCcaCommand;
+import ccamanager.command.ViewCcaCommand;
+import ccamanager.command.ViewResidentCommand;
+import ccamanager.command.ViewPointsCommand;
 
 /**
  * Parser — reads raw user input and returns the appropriate Command object.
