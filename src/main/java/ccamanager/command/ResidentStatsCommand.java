4fa6bd4990659ab76e4c469f6aaa4107a2ddbfbf
Rehaan Mahmood <rehaanmd.310506@gmail.com>
2026-03-29 13:10:57 +0800

    feat: compute total points for each resident

--- src/main/java/ccamanager/command/ResidentStatsCommand.java
+++ src/main/java/ccamanager/command/ResidentStatsCommand.java
@@ -3,12 +3,35 @@ package ccamanager.command;
 import ccamanager.manager.CcaManager;
 import ccamanager.manager.EventManager;
 import ccamanager.manager.ResidentManager;
+import ccamanager.model.Resident;
 import ccamanager.ui.Ui;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ResidentStatsCommand extends Command {
 
     @Override
     public void execute(CcaManager ccaManager, ResidentManager residentManager, EventManager eventManager, Ui ui) {
+        ArrayList<Resident> residents = residentManager.getResidentList();
+        if (residents.isEmpty()) {
+            ui.showMessage("There are no residents currently. Please add residents using add-resident command");
+            return;
+        }
+        HashMap<Resident, Integer> totalPoints = totalPoints(residents);
+    }
 
+    private static HashMap<Resident, Integer> totalPoints(ArrayList<Resident> residents) {
+        HashMap<Resident, Integer> totalPoints = new HashMap<>();
+        for (Resident resident : residents) {
+            ArrayList<Integer> points = resident.getPoints();
+            if (points.isEmpty()) {
+                totalPoints.put(resident, 0);
+            } else {
+                int sum = points.stream().mapToInt(Integer::intValue).sum();
+                totalPoints.put(resident, sum);
+            }
+        }
+        return totalPoints;
     }
 }
